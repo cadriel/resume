12fce5cfd6150c251a1b700741cd9bc8828764ea
Craig Bassett <craig.bassett@gmail.com>
2023-10-01 15:34:39 -0500

    (fix) simplify by removing slug

--- app.vue
+++ app.vue
@@ -1,6 +1,25 @@
 <template>
   <!-- <div class="text-gray-700 dark:text-gray-300 font-sans min-h-screen container"> -->
   <div class="container text-gray-700 dark:text-gray-300 font-sans min-h-screen bg-gray-100 dark:bg-gray-800">
-    <NuxtPage />
+     <div class="w-full flex min-h-screen flex-col sm:flex-row flex-wrap sm:flex-nowrap flex-grow">
+      <Left />
+      <Main />
+    </div>
   </div>
 </template>
+
+<style lang="postcss" scoped>
+.badge {
+  @apply inline-block bg-gray-200 dark:bg-gray-950 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-400;
+  &:hover {
+    @apply bg-gray-300;
+  }
+}
+
+@media (min-width: 640px) {
+  .w-fixed {
+    flex: 0 1 280px;
+    min-width: 280px;
+  }
+}
+</style>
