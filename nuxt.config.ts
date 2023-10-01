3f5a68e11029027ef55f06fbcf2fff2b1d9e2158
Craig Bassett <craig.bassett@gmail.com>
2023-10-01 15:11:53 -0500

    (feat) gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,6 +33,10 @@ export default defineNuxtConfig({
       }
     }
   },
+  app: {
+    baseURL: '/resume/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
   ssr: false,
   devtools: { enabled: true }
 })
